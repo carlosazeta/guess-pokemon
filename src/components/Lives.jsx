31c47ae9e73fe40205089ef905b4173afb3728e1
carlosazeta <carlosagundezdev@gmail.com>
2023-11-07 16:21:59 +0100

    Add lives counter and icons

--- src/components/Lives.jsx
+++ src/components/Lives.jsx
@@ -1,10 +1,20 @@
-export function Lives () {
+export function Lives ({ lives }) {
+  const heartIcons = []
+
+  // Llena el array con los iconos de corazones completos al inicio
+  for (let i = 0; i < 3; i++) {
+    if (i < lives) {
+      // Iconos de corazones completos si quedan vidas
+      heartIcons.push(<i key={i} className='nes-icon is-large heart' />)
+    } else {
+      // Iconos de corazones vacíos si no quedan vidas
+      heartIcons.push(<i key={i} className='nes-icon is-large is-transparent heart' />)
+    }
+  }
+
   return (
     <section className='icon-list'>
-      <i className='nes-icon is-large heart' />
-      <i className='nes-icon is-large is-half heart' />
-      <i className='nes-icon is-large is-transparent heart' />
-      <i className='nes-icon is-large heart is-empty' />
+      {heartIcons}
     </section>
   )
 }
--- src/components/Pokemon.jsx
+++ src/components/Pokemon.jsx
@@ -4,10 +4,23 @@ import { Lives } from './Lives'
 export function Pokemon ({ id, image, name, loadNewPokemon }) {
   const [unlockPokemon, setUnlockPokemon] = useState(false)
   const [userGuess, setUserGuess] = useState('')
+  const [lives, setLives] = useState(3)
 
   const handleGuessPokemon = () => {
     const cleanUserGuess = userGuess.trim().toLowerCase()
     const cleanPokemonName = name.trim().toLowerCase()
+
+    if (userGuess === '') return
+
+    setLives((prevLives) => {
+      const newLives = prevLives - 1
+
+      if (newLives === 0) {
+        setUnlockPokemon(true)
+      }
+
+      return newLives
+    })
 
     if (cleanUserGuess === cleanPokemonName) {
       setUnlockPokemon(true)
@@ -22,6 +35,7 @@ export function Pokemon ({ id, image, name, loadNewPokemon }) {
     setUnlockPokemon(false)
     setUserGuess('')
     loadNewPokemon()
+    setLives(3)
   }
 
   return (
@@ -55,7 +69,9 @@ export function Pokemon ({ id, image, name, loadNewPokemon }) {
         onClick={handleResetGame}
       >Play again
       </button>
-      <Lives />
+      <Lives
+        lives={lives}
+      />
     </div>
   )
 }
